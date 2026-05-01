4f6340d58495188616a280b04ac4ec4c890097d9
wzqany-source <wzqany@gmail.com>
2026-05-01 22:04:11 +0800

    脚本工程化实现0error

--- scripts/web-srv/log_analysis.sh
+++ scripts/web-srv/log_analysis.sh
@@ -4,11 +4,11 @@ LOG_FILE="/var/log/nginx/access.log"
 
 # 1. 第一步：Shell 进行初筛（数数）
 # 统计最近的 404 和 500 错误数量
-HTTP_404=$(awk '{print $9}' $LOG_FILE | grep "404" | wc -l)
-HTTP_500=$(awk '{print $9}' $LOG_FILE | grep "500" | wc -l)
+HTTP_404=$(awk '{print $9}' $LOG_FILE | grep -c "404" )
+HTTP_500=$(awk '{print $9}' $LOG_FILE | grep -c "500" )
 
 # 2. 第二步：判断是否达到触发 Python 的条件
-if [ $HTTP_404 -gt 5 ] || [ $HTTP_500 -gt 0 ]; then
+if [ "$HTTP_404" -gt 5 ] || [ "$HTTP_500" -gt 0 ]; then
     echo "【系统信号】检测到异常，正在唤醒 AI 专家 (Python) 进行深度分析..."
     
     # --- 核心连接点：Shell 在这里拉起 Python ---
